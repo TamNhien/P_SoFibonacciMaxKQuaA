9542b7aa3b99b0e4c35af77273eaee183d81adca
TamNhien <canhthien91@gmail.com>
2024-03-26 19:17:46 +0700

    Add project files.

---
+++ P_SoFibonacciMaxKQuaA.py
@@ -0,0 +1,8 @@
+﻿A = int(input("Nhập số A: "))
+fib_prev, fib_curr = 1, 1
+
+while fib_curr <= A:
+    fib_prev, fib_curr = fib_curr, fib_curr + fib_prev
+
+print("Số Fibonacci lớn nhất nhưng không vượt quá A là : ", fib_prev)
+
